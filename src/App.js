93ce62ced0e8813f8d839a0d3ad4041f4b2571cf
anotherjsguy <kuldeepkeshwar@gmail.com>
2020-07-27 10:03:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Stack } from "layout-ui";
 
 export default function App() {
   return (
-      <Stack gap="2rem" align="center">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-      </Stack>
+    <Stack gap="2rem" align="center">
+      <div>Item 1</div>
+      <div>Item 2</div>
+    </Stack>
   );
 }
